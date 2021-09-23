027f6a4d4500379fcc876f3bc3dc8f9a88a1eb23
DaviRain-Su <davirain.yin@gamil.com>
2021-09-23 16:23:10 +0800

    Update ...

--- src/event.rs
+++ src/event.rs
@@ -241,7 +241,8 @@ pub mod primitive {
                 destination_channel: self.destination_channel.to_ibc_channel_id(),
                 data: self.data,
                 timeout_height: self.timeout_height.to_ibc_height(),
-                timeout_timestamp: self.timeout_timestamp.to_ibc_timestamp(),
+                timeout_timestamp: IbcTimestamp::now(),
+                // timeout_timestamp: self.timeout_timestamp.to_ibc_timestamp(),
             }
         }
     }
