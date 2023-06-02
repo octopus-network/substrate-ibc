ef6186efd275c41309293c8543879645ceac0ced
Davirain <davirain.yin@gmail.com>
2023-06-03 01:03:30 +0800

    Add query_application_status function

--- pallet-ibc-rpc/src/ibc_core/mod.rs
+++ pallet-ibc-rpc/src/ibc_core/mod.rs
@@ -1,9 +1,45 @@
+use crate::node_template;
 use anyhow::Result;
-use ibc_relayer::chain::endpoint::{ChainEndpoint, ChainStatus, HealthCheck};
+use ibc::core::timestamp::Timestamp;
+use std::time::Duration;
+use subxt::{OnlineClient, SubstrateConfig};
+
+#[derive(Debug)]
+pub struct Height {
+	pub reversion_number: u64,
+	pub reversion_height: u64,
+}
+
+#[derive(Debug)]
+pub struct ChainStatus {
+	pub height: Height,
+	pub timestamp: Timestamp,
+}
 
 /// Query the latest height and timestamp the application is at
-fn query_application_status() -> Result<ChainStatus> {
-	todo!()
+pub async fn query_application_status(
+	rpc_client: OnlineClient<SubstrateConfig>,
+) -> Result<ChainStatus> {
+	let finalized_header_hash = rpc_client.rpc().finalized_head().await?;
+	let finalized_header = rpc_client
+		.rpc()
+		.block(Some(finalized_header_hash))
+		.await?
+		.ok_or(anyhow::anyhow!("Block is None"))?;
+	let block_number = finalized_header.block.header.number;
+	let height = Height { reversion_number: 0, reversion_height: block_number as u64 };
+	let timestamp_storage_query = node_template::storage().timestamp().now();
+	let result = rpc_client
+		.storage()
+		.at_latest()
+		.await?
+		.fetch(&timestamp_storage_query)
+		.await?
+		.ok_or(anyhow::anyhow!("Timestamp is None"))?;
+	let duration = Duration::from_millis(result);
+	let timestamp = Timestamp::from_nanoseconds(duration.as_nanos() as u64)
+		.map_err(|e| anyhow::anyhow!("get timestmap eror({})", e))?;
+	Ok(ChainStatus { height, timestamp })
 }
 
 #[cfg(test)]
@@ -11,5 +47,10 @@ mod tests {
 	use super::*;
 
 	#[tokio::test]
-	async fn test_query_application_status() {}
+	async fn test_query_application_status() {
+		// Create a new API client, configured to talk to Polkadot nodes.
+		let api = OnlineClient::<SubstrateConfig>::new().await.unwrap();
+		let result = query_application_status(api).await.unwrap();
+		println!("{:?}", result);
+	}
 }
--- pallet-ibc-rpc/src/lib.rs
+++ pallet-ibc-rpc/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod ibc_core;
 
+#[subxt::subxt(runtime_metadata_path = "metadata/metadata.scale")]
+pub mod node_template {}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
